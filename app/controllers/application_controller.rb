07dc5780c1af7e9ad9e651bb6b0107524acdcdef
nizutaro <niduka81@gmail.com>
2020-09-27 02:20:03 +0900

    [add]management_system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-      projects_path(resource) # ログイン後に遷移するpathを設定
+      
+      user_path(resource) # ログイン後に遷移するpathを設定
     end
 
     def after_inactive_sign_up_path_for(resource)
-      projects_path(resource)                    #ここを修正する
+      user_path(resource)
     end
   protected
     def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
  
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions'
+}
   get 'jobs/index'
   get 'jobs/edit'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index,:show,:create,:edit, :update, :destroy] do
     resources :nicetry, only: [:create,:destroy]
   end
-  
+
   resources :jobs, only: [:index,:create, :edit, :update, :destroy]
-  
 
   resources :projects do
     resource :favorites
     resources :comments, only: [:create, :destroy]
   end
+
   root 'homes#about'
   get '/homes/inquery' => 'homes#inquery'
-  
+
 
 end
