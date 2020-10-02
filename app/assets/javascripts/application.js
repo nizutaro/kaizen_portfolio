7f3d3c3867cfaef3a10a4b5bf6e0761744fa5485
nizutaro <niduka81@gmail.com>
2020-10-02 20:40:04 +0900

    [add]skkipr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,17 @@
 //= require turbolinks
 //= require_tree .
 //= require Chart.min
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    transition: "slide",
+    speed: 1000,
+    easing: "easeOutQuart",
+    navType: "block",
+    childrenElementType: "div",
+    arrows: true,
+    autoPlay: true,
+    autoPlayDuration: 3000,
+    keyboardOnAlways: true,
+    hidePrevious: false,
+  });
+});
