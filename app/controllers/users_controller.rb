ca8ada03c11e6c3ee2b41335cdb900826ee5aacf
nizutaro <niduka81@gmail.com>
2020-10-10 01:23:03 +0900

    [add]flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
   def index
-    #@users = User.page(params[:page]).reverse_order
-    #@users = User.where(activated: true).paginate(page: params[:page]).search(params[:search])
     @users = User.all.page(params[:page]).search(params[:search])
   end
 
   def show
     @user = User.find(params[:id])
-    #@projects = @user.projects
     @projects_count = Project.where(user_id: @user).count
-
     @projects = @user.projects.order("created_at DESC").page(params[:page]).per(10)
-
-
   end
 
   def edit
@@ -25,8 +19,9 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)  
-      redirect_to user_path(current_user)
+    if @user.update(user_params)
+      flash[:success] = 'プロフィールを更新しました'
+      redirect_to user_path(@user)
     else
       @user = current_user
       render :edit 
